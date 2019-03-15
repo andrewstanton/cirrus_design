d6441cd202c17416c6de079c98894186f0486376
Andrew Stanton <andrew@toddprod.com>
2019-03-15 14:04:57 -0400

    Updating nav to have sub menus off of sub menus

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,8 +1,25 @@
 import React from 'react'
 
+const renderItems = (items) => {
+    return items.map(i => {
+
+    });
+};
+
 const Banner = () => (
     <section className="banner">
         
+        <div className="banner_slide">
+            <div className="banner_slide_overlay">
+                <h1 className="banner_slide_title">Quality Design, Done Right The First Time</h1>
+                <h2 className="banner_slide_subtitle">Here is another little title that goes another here</h2>
+                <p className="banner_slide_text">hkjhdkjfh skdjfh ksjdhfksjhd fkjshd kfjshd kfjhsdkjfhskdjhf ksjdh fksjdh fkjsh dfkjsh dfkjhs ddkfj  hskdhfskdjhf ksjdhf kjsdhfkjshd kfjhs dkfjhs dkjfh skdjhf</p>
+                <button className="btn btn--default btn--margin-top">
+                    Discover Our 3D Services
+                </button>
+            </div>
+        </div>
+
     </section>
 )
 
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -38,23 +38,103 @@ const Header = () => (
                         <nav className="m-nav">
                             <ul className="m-nav_menu">
                                 <li>
-                                    <Link to="/">Home</Link>
+                                    <Link to="/">3D Scanning</Link>
                                     <div className="m-nav_dd">
                                         <ul>
                                             <li>
-                                                <Link to="/">Sub Link</Link>
+                                                <Link to="/">Industrial Plants</Link>
                                             </li>
                                             <li>
-                                                <Link to="/">Sub Link 2</Link>
+                                                <Link to="/">Arch Eng Const (AEC)</Link>
+                                            </li>
+                                            <li>
+                                                <Link to="/">Real Estate</Link>
+                                            </li>
+                                            <li>
+                                                <Link to="/">Reverse Engineering</Link>
+                                            </li>
+                                            <li>
+                                                <Link to="/">Conceptual Design + Testing</Link>
                                             </li>
                                         </ul>
                                     </div>
                                 </li>
                                 <li>
-                                    <Link to="/">About Us</Link>
+                                    <Link to="/">3D Modeling</Link>
+                                    <div className="m-nav_dd">
+                                        <ul>
+                                            <li>
+                                                <Link to="/">3D Modeling Anything</Link>
+                                                <div className="m-nav_dd">
+                                                    <ul>
+                                                        <li>
+                                                            <Link to="/">Using / ACAD + SW</Link>
+                                                        </li>
+                                                    </ul>
+                                                </div>
+                                            </li>
+                                            <li>
+                                                <Link to="/">3D Printing</Link>
+                                            </li>
+                                        </ul>
+                                    </div>
                                 </li>
                                 <li>
-                                    <Link to="/">Contact Us</Link>
+                                    <Link to="/">Engineering</Link>
+                                    <div className="m-nav_dd">
+                                        <ul>
+                                            <li>
+                                                <Link to="/">Hydro</Link>
+                                            </li>
+                                            <li>
+                                                <Link to="/">Mechanical</Link>
+                                            </li>
+                                            <li>
+                                                <Link to="/">Electrical</Link>
+                                            </li>
+                                            <li>
+                                                <Link to="/">PID's + Machine Controls</Link>
+                                            </li>
+                                            <li>
+                                                <Link to="/">Structural</Link>
+                                            </li>
+                                            <li>
+                                                <Link to="/">Other Services</Link>
+                                                <div className="m-nav_dd">
+                                                    <ul>
+                                                        <li>
+                                                            <Link to="/">2D Document Scanning</Link>
+                                                        </li>
+                                                        <li>
+                                                            <Link to="/">Project Management</Link>
+                                                        </li>
+                                                        <li>
+                                                            <Link to="/">FEA In Inventor 2019</Link>
+                                                        </li>
+                                                    </ul>
+                                                </div>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </li>
+                                <li>
+                                    <Link to="/">About</Link>
+                                    <div className="m-nav_dd">
+                                        <ul>
+                                            <li>
+                                                <Link to="/">Mission</Link>
+                                            </li>
+                                            <li>
+                                                <Link to="/">Equipment</Link>
+                                            </li>
+                                            <li>
+                                                <Link to="/">Case Study</Link>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </li>
+                                <li>
+                                    <Link to="/">Contact</Link>
                                 </li>
                             </ul> 
                         </nav>
