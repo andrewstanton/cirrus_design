cfbc0b832063da9cd6682bef0dbc98ab867b7c92
Andrew <andrew@toddprod.com>
2019-04-16 08:34:10 -0400

    Updating react component animations

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,172 +1,242 @@
-import React from 'react'
-import SocialIcons from '../SocialIcons'
-import { StaticQuery, graphql, Link } from 'gatsby'
+import React from "react";
+import SocialIcons from "../SocialIcons";
+import { StaticQuery, graphql, Link } from "gatsby";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
-import logo from '../../img/logo.png';
+import logo from "../../img/logo.png";
 
 class Header extends React.Component {
-    state = {
-        toggle: false
-    };
+  state = {
+    toggle: false,
+  };
 
-    renderMenuClass = () => {
-        const navMenu = this.state.toggle ? `m-nav_menu--openmenu` : '';
-        return `m-nav_menu ${ navMenu }`;
+  constructor(props) {
+    super(props);
+    this.headerRef = React.createRef();
+  }
+
+  componentWillMount() {
+    this.addEvents();
+  }
+
+  componentWillUnmount() {
+    this.removeEvents();
+  }
+
+  addEvents = () => {
+    window.addEventListener("scroll", this.handleScroll);
+  };
+
+  removeEvents = () => {
+    window.removeEventListener("scroll", this.handleScroll);
+  };
+
+  handleScroll = () => {
+    const scrollTarget = window,
+      stickyDiv = this.headerRef.current || null,
+      scrollRect = {
+        height: scrollTarget.innerHeight,
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        x: scrollTarget.scrollX,
+        y: scrollTarget.scrollY,
+      };
+
+    // If no sticky ref set
+    if (!stickyDiv) {
+      return;
     }
+    const stickyRect = stickyDiv.getBoundingClientRect();
+    const stuckTop = scrollRect.y > stickyRect.height;
+    this.setState({ stuckTop });
+  };
 
-    render() {
-        const sticky = require('../../scripts/sticky-header');
-        return (
-            <StaticQuery 
-                query={ graphql`
-                    query HeaderQuery {
-                        site {
-                            siteMetadata {
-                                title
-                            }
-                        }
+  renderMenuClass = () => {
+    const navMenu = this.state.toggle ? `m-nav_menu--openmenu` : "";
+    return `m-nav_menu ${navMenu}`;
+  };
+
+  render() {
+    return (
+      <StaticQuery
+        query={graphql`
+          query HeaderQuery {
+            site {
+              siteMetadata {
+                title
+              }
+            }
+          }
+        `}
+        render={data => {
+          return (
+            <header
+              className={`s-header ${this.state.stuckTop &&
+                "s-header--sticky"}`}
+              ref={this.headerRef}
+            >
+              <section className="s-header_top">
+                <div className="wrapper">
+                  <div className="s-header_social">
+                    <SocialIcons />
+                  </div>
+                </div>
+              </section>
+              <section className="s-header_bottom">
+                <div className="wrapper s-header_wrapper">
+                  <button
+                    className="btn--link s-header_menuButton"
+                    onClick={() =>
+                      this.setState({ toggle: !this.state.toggle })
                     }
-                `}
-                render={ data => {
-                    return (
-                        <header className="s-header">
-                            <section className="s-header_top">
-                                <div className="wrapper">
-                                    <div className="s-header_social">
-                                        <SocialIcons />
-                                    </div>
-                                </div>
-                            </section>
-                            <section className="s-header_bottom">
-                                <div className="wrapper s-header_wrapper">
-        
-                                    <button className="btn--link s-header_menuButton" onClick={ () => this.setState({ toggle: !this.state.toggle }) }>
-                                        <FontAwesomeIcon icon={faBars} />
-                                    </button>
-        
-                                    <div className="s-header_logo">
-                                        <Link to="/">
-                                            <img className="s-header_logo_img"
-                                                src={ logo }
-                                                alt={ data.site.siteMetadata.title } />
-                                        </Link>
-                                    </div>
-        
-                                    <nav className="m-nav">
-                                        <ul className={ this.renderMenuClass() }>
-                                            <li>
-                                                <Link to="/3d-scanning">3D Scanning</Link>
-                                                <div className="m-nav_dd">
-                                                    <ul>
-                                                        <li>
-                                                            <Link to="/services/industrial-plants">Industrial Plants</Link>
-                                                        </li>
-                                                        <li>
-                                                            <Link to="/services/aec">Arch Eng Const (AEC)</Link>
-                                                        </li>
-                                                        <li>
-                                                            <Link to="/services/real-estate">Real Estate</Link>
-                                                        </li>
-                                                        <li>
-                                                            <Link to="/services/reverse-engineering">Reverse Engineering</Link>
-                                                        </li>
-                                                        <li>
-                                                            <Link to="/services/conceptual-design-testing">Conceptual Design + Testing</Link>
-                                                        </li>
-                                                    </ul>
-                                                </div>
-                                            </li>
-                                            <li>
-                                                <Link to="/3d-modeling-printing">3D Modeling</Link>
-                                                <div className="m-nav_dd">
-                                                    <ul>
-                                                        <li>
-                                                            <Link to="/services/3d-modeling">3D Modeling Anything</Link>
-                                                            <div className="m-nav_dd">
-                                                                <ul>
-                                                                    <li>
-                                                                        <Link to="/services/acad-sw">Using / ACAD + SW</Link>
-                                                                    </li>
-                                                                </ul>
-                                                            </div>
-                                                        </li>
-                                                        <li>
-                                                            <Link to="/services/3d-printing">3D Printing</Link>
-                                                        </li>
-                                                    </ul>
-                                                </div>
-                                            </li>
-                                            <li>
-                                                <Link to="/engineering-design-drafting">Engineering</Link>
-                                                <div className="m-nav_dd">
-                                                    <ul>
-                                                        <li>
-                                                            <Link to="/services/hydro">Hydro</Link>
-                                                        </li>
-                                                        <li>
-                                                            <Link to="/services/mechanical">Mechanical</Link>
-                                                        </li>
-                                                        <li>
-                                                            <Link to="/services/electrical">Electrical</Link>
-                                                        </li>
-                                                        <li>
-                                                            <Link to="/services/pid-machine-controls">PID's + Machine Controls</Link>
-                                                        </li>
-                                                        <li>
-                                                            <Link to="/services/structural">Structural</Link>
-                                                        </li>
-                                                        <li>
-                                                            <Link to="/services/other-services">Other Services</Link>
-                                                            <div className="m-nav_dd">
-                                                                <ul>
-                                                                    <li>
-                                                                        <Link to="/services/2d-document-scanning">2D Document Scanning</Link>
-                                                                    </li>
-                                                                    <li>
-                                                                        <Link to="/services/project-management">Project Management</Link>
-                                                                    </li>
-                                                                    <li>
-                                                                        <Link to="/services/fea-in-inventor-2019">FEA In Inventor 2019</Link>
-                                                                    </li>
-                                                                </ul>
-                                                            </div>
-                                                        </li>
-                                                    </ul>
-                                                </div>
-                                            </li>
-                                            <li>
-                                                <Link to="/about">About</Link>
-                                                <div className="m-nav_dd">
-                                                    <ul>
-                                                        <li>
-                                                            <Link to="/about/mission">Mission</Link>
-                                                        </li>
-                                                        <li>
-                                                            <Link to="/about/equipment">Equipment</Link>
-                                                        </li>
-                                                        <li>
-                                                            <Link to="/about/case-study">Case Study</Link>
-                                                        </li>
-                                                    </ul>
-                                                </div>
-                                            </li>
-                                            <li>
-                                                <Link to="/contact">Contact</Link>
-                                            </li>
-                                        </ul> 
-                                    </nav>
-        
-                                </div>
-                            </section>
-                        </header>
-                    );
-                }}
-            />
-        )       
-    }
+                  >
+                    <FontAwesomeIcon icon={faBars} />
+                  </button>
+
+                  <div className="s-header_logo">
+                    <Link to="/">
+                      <img
+                        className="s-header_logo_img"
+                        src={logo}
+                        alt={data.site.siteMetadata.title}
+                      />
+                    </Link>
+                  </div>
+
+                  <nav className="m-nav">
+                    <ul className={this.renderMenuClass()}>
+                      <li>
+                        <Link to="/3d-scanning">3D Scanning</Link>
+                        <div className="m-nav_dd">
+                          <ul>
+                            <li>
+                              <Link to="/services/industrial-plants">
+                                Industrial Plants
+                              </Link>
+                            </li>
+                            <li>
+                              <Link to="/services/aec">
+                                Arch Eng Const (AEC)
+                              </Link>
+                            </li>
+                            <li>
+                              <Link to="/services/real-estate">
+                                Real Estate
+                              </Link>
+                            </li>
+                            <li>
+                              <Link to="/services/reverse-engineering">
+                                Reverse Engineering
+                              </Link>
+                            </li>
+                            <li>
+                              <Link to="/services/conceptual-design-testing">
+                                Conceptual Design + Testing
+                              </Link>
+                            </li>
+                          </ul>
+                        </div>
+                      </li>
+                      <li>
+                        <Link to="/3d-modeling-printing">3D Modeling</Link>
+                        <div className="m-nav_dd">
+                          <ul>
+                            <li>
+                              <Link to="/services/3d-modeling">
+                                3D Modeling Anything
+                              </Link>
+                            </li>
+                            <li>
+                              <Link to="/services/3d-printing">
+                                3D Printing
+                              </Link>
+                            </li>
+                          </ul>
+                        </div>
+                      </li>
+                      <li>
+                        <Link to="/engineering-design-drafting">
+                          Engineering
+                        </Link>
+                        <div className="m-nav_dd">
+                          <ul>
+                            <li>
+                              <Link to="/services/hydro">Hydro</Link>
+                            </li>
+                            <li>
+                              <Link to="/services/mechanical">Mechanical</Link>
+                            </li>
+                            <li>
+                              <Link to="/services/electrical">Electrical</Link>
+                            </li>
+                            <li>
+                              <Link to="/services/pid-machine-controls">
+                                PID's + Machine Controls
+                              </Link>
+                            </li>
+                            <li>
+                              <Link to="/services/structural">Structural</Link>
+                            </li>
+                            <li>
+                              <Link to="/services/other-services">
+                                Other Services
+                              </Link>
+                              <div className="m-nav_dd">
+                                <ul>
+                                  <li>
+                                    <Link to="/services/2d-document-scanning">
+                                      2D Document Scanning
+                                    </Link>
+                                  </li>
+                                  <li>
+                                    <Link to="/services/project-management">
+                                      Project Management
+                                    </Link>
+                                  </li>
+                                  <li>
+                                    <Link to="/services/fea-in-inventor-2019">
+                                      FEA In Inventor 2019
+                                    </Link>
+                                  </li>
+                                </ul>
+                              </div>
+                            </li>
+                          </ul>
+                        </div>
+                      </li>
+                      <li>
+                        <Link to="/about">About</Link>
+                        <div className="m-nav_dd">
+                          <ul>
+                            <li>
+                              <Link to="/about/mission">Mission</Link>
+                            </li>
+                            <li>
+                              <Link to="/about/equipment">Equipment</Link>
+                            </li>
+                            <li>
+                              <Link to="/about/case-study">Case Study</Link>
+                            </li>
+                          </ul>
+                        </div>
+                      </li>
+                      <li>
+                        <Link to="/contact">Contact</Link>
+                      </li>
+                    </ul>
+                  </nav>
+                </div>
+              </section>
+            </header>
+          );
+        }}
+      />
+    );
+  }
 }
 
-export default Header
+export default Header;
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,111 +1,99 @@
-import React from 'react'
-import { graphql } from 'gatsby'
-import Helmet from 'react-helmet'
+import React from "react";
+import { graphql } from "gatsby";
+import Helmet from "react-helmet";
 
-import Layout from '../components/Layout'
-import Card from '../components/Card'
-import Banner from '../components/Banner'
-import image from '../img/cardImage.jpg'
+import Layout from "../components/Layout";
+import Card from "../components/Card";
+import Banner from "../components/Banner";
+import image from "../img/cardImage.jpg";
 
-const renderBlocks = (homeblocks) => {
+const renderBlocks = homeblocks => {
   return homeblocks.map((block, ix) => (
-    <Card 
-      title={ block.title }
-      small={ true }
-      image={ block.image }
-      key={ix}
-    />
+    <Card title={block.title} small={true} image={block.image} key={ix} />
   ));
 };
 
-const renderSections = (homesections) => {
+const renderSections = homesections => {
   return homesections.map((block, ix) => (
-    <Card 
-      title={ block.title }
-      subtitle={ block.subtitle }
-      buttonText={ block.buttontext }
-      buttonLink={block.buttonlink }
-      image={ block.image }
-      large={ true }
-      key={ix}>
-      { block.description } 
+    <Card
+      title={block.title}
+      subtitle={block.subtitle}
+      buttonText={block.buttontext}
+      buttonLink={block.buttonlink}
+      image={block.image}
+      large={true}
+      key={ix}
+    >
+      {block.description}
     </Card>
   ));
-}
+};
 
-const IndexPageTemplate = ({slides, homeblocks, homesections, aboutsection}) => (
+const IndexPageTemplate = ({
+  slides,
+  homeblocks,
+  homesections,
+  aboutsection,
+}) => (
   <div className="s-body">
-    
-    <Banner slides={ slides } />
-    
-    {
-    
-      homeblocks ? 
-      <div className="s-body_card-container wrapper">
-        { renderBlocks(homeblocks) }
-      </div> : null
-    
-    }
+    <Banner slides={slides} />
 
-    {
-      homesections ?
+    {homesections && (
       <div className="s-body_card-container s-body_card-container--dark">
-        { renderSections(homesections) }
-      </div> : null
-    }
+        {renderSections(homesections)}
+      </div>
+    )}
 
-    {
-      aboutsection ?
+    {homeblocks && (
+      <div className="s-body_card-container wrapper">
+        {renderBlocks(homeblocks)}
+      </div>
+    )}
+
+    {aboutsection && (
       <div className="wrapper">
-        <Card 
-          title={ aboutsection.title }
-          image={ aboutsection.image }
-          subtitle={ aboutsection.subtitle }
-          buttonText={ aboutsection.buttontext }
-          buttonLink={ aboutsection.buttonlink }
-          horizontal={ true }>
-          { aboutsection.description }
+        <Card
+          title={aboutsection.title}
+          image={aboutsection.image}
+          subtitle={aboutsection.subtitle}
+          buttonText={aboutsection.buttontext}
+          buttonLink={aboutsection.buttonlink}
+          horizontal={true}
+        >
+          {aboutsection.description}
         </Card>
-      </div> : null
-    }
-
+      </div>
+    )}
   </div>
-)
+);
 
-const IndexPage = ({data}) => {
-  const { frontmatter } = data.markdownRemark
+const IndexPage = ({ data }) => {
+  const { frontmatter } = data.markdownRemark;
   const { siteMetadata } = data.site;
 
   return (
     <Layout>
-      
       <Helmet>
-        <title>{`${ siteMetadata.title }`}</title>
-        <meta
-            name="description"
-            content={`${ siteMetadata.description }`}
-        />
+        <title>{`${siteMetadata.title}`}</title>
+        <meta name="description" content={`${siteMetadata.description}`} />
       </Helmet>
 
-      <IndexPageTemplate
-        { ...frontmatter }
-      />
-      
+      <IndexPageTemplate {...frontmatter} />
     </Layout>
   );
-}
+};
 
-export default IndexPage
+export default IndexPage;
 
 export const pageQuery = graphql`
-query IndexPageTemplate {
-  site {
-    siteMetadata {
-      title
-      description
+  query IndexPageTemplate {
+    site {
+      siteMetadata {
+        title
+        description
+      }
     }
-  }
-  markdownRemark(frontmatter: {templateKey: {eq: "index-page"}}) {
+    markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
       frontmatter {
         templateKey
         slides {
@@ -135,8 +123,8 @@ query IndexPageTemplate {
           subtitle
           title
           buttonlink
-        } 
+        }
       }
+    }
   }
-}
-`
+`;
